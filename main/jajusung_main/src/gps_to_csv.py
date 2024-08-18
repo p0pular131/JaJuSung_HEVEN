19da72f151f2c19da55292af13bcd4864bb3a1df
yyonyy <yw8431@g.skku.edu>
2024-08-18 16:55:39 +0900

    gps_loggin용 파일 생성

---
+++ main/jajusung_main/src/gps_to_csv.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import rospy
+import csv
+from morai_msgs.msg import GPSMessage
+from math import *
+
+csv_file_path = 'gps_test.csv'
+
+data_list = []
+
+CNT = 2
+
+class GpsTracker():
+    def __init__(self):
+        self.cnt = 0
+        self.idx_cnt = 0
+        
+        rospy.init_node("recording_node")
+        rospy.sleep(1)
+        rospy.Subscriber("/gps", GPSMessage, self._gps_callback)
+        rospy.loginfo("Gps Tracker Node on. Waiting for topics...")
+
+        rospy.spin()
+
+    # Functions
+    def _gps_callback(self, data=GPSMessage):
+        if self.cnt % CNT == 0:
+            data_list.append([data.latitude, data.longitude])
+            rospy.loginfo("Get row #%3d.", self.idx_cnt / CNT)
+        self.idx_cnt += 1
+        self.cnt = (self.cnt + 1) % CNT
+
+
+if __name__ == "__main__":
+    try:
+        GpsTracker()
+    except rospy.ROSInterruptException:
+        pass
+
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+
+        for data in data_list:
+            a_data, b_data = data  # 데이터를 a와 b 열로 분리
+            csv_writer.writerow([a_data, b_data])  # CSV 파일에 추가하기
+
+    print("Write done!")
